06619fd597675b7a26b812d87b64e68c73d8086c
Midland digital <mekarosi@midlanddigitalsolutions.com>
2021-10-10 20:27:13 +0100

    Add more features to the face

--- src/App.js
+++ src/App.js
@@ -3,10 +3,27 @@ import './App.css';
 
 function App() {
   return (
-    <div id="cartoon">
+    <div id="cartoon" role="img" aria-labelledby="description">
+      <div id="description">Cartoon of a young man with curly hair</div>
+      {/* <div className="hair-neck"></div> */}
+      <div className="body"></div>
+      <div class="ear"></div>
+      <div class="ear"></div>
+      <div className="neck"></div>
+      <div className="hair-back"></div>
       <div className="face">
+      <div className="cheeks"></div>
+        <div className="eye">
+          <div className="pupil"></div>
+        </div>
+        <div className="eye">
+          <div className="pupil"></div>
+        </div>
         <div className="month"></div>
-      </div>   
+        <div className="freckles"></div>
+        <div className="nose"></div>
+      </div>  
+      <div className="hair-top"></div> 
     </div>
   );
 }
